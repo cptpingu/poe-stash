56efbfa472c5c0185da2c5c1f0b6e7665ea87d36
Axel Berardino <axel.berardino@gmail.com>
2019-08-12 01:43:08 +0200

    Remove hardcoded passwords and use an external password file for the server part

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"html/template"
+	"io"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 
@@ -13,7 +17,7 @@ import (
 )
 
 // setupRouter setups the http server and all its pages.
-func setupRouter() *gin.Engine {
+func setupRouter(passwords map[string]string) *gin.Engine {
 	router := gin.Default()
 
 	t := template.Must(generate.LoadAllTemplates())
@@ -24,13 +28,47 @@ func setupRouter() *gin.Engine {
 	router.GET("/", page.MainPageHandler)
 	router.GET("/view/:account", page.ViewAccountHandler)
 
-	authorized := router.Group("/", gin.BasicAuth(gin.Accounts{
-		"***":  "***",
-		"****": "****",
-	}))
+	authorized := router.Group("/", gin.BasicAuth(passwords))
 	authorized.GET("/gen/:account", page.GenAccountHandler)
 
 	return router
+}
+
+// loadPasswords load passwords from a given file.
+// Format is:
+//   login:pass
+//   login:pass
+//   login:pass
+//   ...
+func loadPasswords(filename string) (r map[string]string, mainErr error) {
+	res := make(map[string]string, 2)
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := file.Close()
+		if err == nil {
+			mainErr = err
+		}
+	}()
+
+	reader := bufio.NewReader(file)
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		parts := strings.Split(string(line), ":")
+		// Invalid line is skipped
+		if len(parts) != 2 {
+			fmt.Println("Skipped invalid line:", string(line))
+		} else {
+			res[parts[0]] = parts[1]
+		}
+	}
+
+	return res, nil
 }
 
 // main is the main routine which launch the http server.
@@ -38,7 +76,13 @@ func setupRouter() *gin.Engine {
 // stash and items for given users.
 func main() {
 	port := flag.Int("port", 2121, "port")
+	passwordFile := flag.String("passwords", "./pass.txt", "password file (containing login:pass in plain text)")
 	flag.Parse()
-	r := setupRouter()
+
+	passwords, err := loadPasswords(*passwordFile)
+	if err != nil {
+		panic(err)
+	}
+	r := setupRouter(passwords)
 	r.Run(fmt.Sprintf(":%d", *port))
 }
